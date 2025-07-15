91a5197ae45cbdbfa0504301dbfab048780d2539
Gaurav Chakravorty <gchak207@gmail.com>
2025-07-15 08:19:16 -0700

    formatting

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""Generative Friending Recommendations.
+
+This package provides tools and models for generating friending recommendations,
+leveraging techniques like next-target prediction and advanced neural network
+architectures.
+"""
